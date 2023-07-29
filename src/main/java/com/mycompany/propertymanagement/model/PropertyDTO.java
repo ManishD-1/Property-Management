f22feddf4bb195470824cc60b08bd3c5943d7600
ManishD-1 <manishdeshpande121@gmail.com>
2023-07-30 01:50:40 +0530

    Added Property DTO file in model

---
+++ src/main/java/com/mycompany/propertymanagement/model/PropertyDTO.java
@@ -0,0 +1,17 @@
+package com.mycompany.propertymanagement.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PropertyDTO {
+
+    private String title;
+    private String Description;
+    private String Name;
+    private String Email;
+    private Double Price;
+    private String Address;
+
+}
